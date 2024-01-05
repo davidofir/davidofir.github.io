4654317a87c66bd9f548c610f049bac662fb860f
ofir <davidof@sheridancollege.ca>
2024-01-05 10:21:26 -0500

    added hamburger menu for responsive design

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,59 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
-
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./styles/navBar.css";
 
 const NavBar = (props) => {
 	const { active } = props;
-
-	return (
-		<React.Fragment>
-			<div className="nav-container">
-				<nav className="navbar">
-					<div className="nav-background">
-						<ul className="nav-list">
-							<li
-								className={
-									active === "home"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/">Home</Link>
-							</li>
-							<li
-								className={
-									active === "projects"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/projects">Projects</Link>
-							</li>
-							<li
-								className={
-									active === "certifications"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/certifications">Certifications</Link>
-							</li>
-							<li
-								className={
-									active === "contact"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/contact">Contact</Link>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		</React.Fragment>
-	);
+	const [isNavExpanded, setIsNavExpanded] = useState(false);
+    return (
+        <React.Fragment>
+            <div className="nav-container">
+                <nav className="navbar">
+                    <div className="menu-icon" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                    <div className={`nav-background ${isNavExpanded ? "expanded" : ""}`}>
+                        <ul className="nav-list">
+                            <li className={active === "home" ? "nav-item active" : "nav-item"}>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className={active === "projects" ? "nav-item active" : "nav-item"}>
+                                <Link to="/projects">Projects</Link>
+                            </li>
+                            <li className={active === "certifications" ? "nav-item active" : "nav-item"}>
+                                <Link to="/certifications">Certifications</Link>
+                            </li>
+                            <li className={active === "contact" ? "nav-item active" : "nav-item"}>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default NavBar;
